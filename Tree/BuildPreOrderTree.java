7212944187d6014321d42801ff93eb2ac78247df
kieransahoo <kkieran50@gmail.com>
2023-06-02 18:59:47 +0530

    common tree questions

--- Tree/BuildPreOrderTree.java
+++ Tree/BuildPreOrderTree.java
@@ -89,6 +89,84 @@ public class BuildPreOrderTree {
 
     }
 
+    public static int countOfNodes(Node root){
+
+        if(root == null){
+            return 0;
+        }
+
+        int leftNodes = countOfNodes(root.left);
+        int rightNodes = countOfNodes(root.right);
+        
+        return leftNodes+rightNodes+1;
+    }
+
+    public static int sumOfNodes(Node root){
+
+        if(root == null){
+            return 0;
+        }
+
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        
+        return leftSum + rightSum + root.data;
+    }
+
+    public static int height(Node root){
+
+        if(root == null){
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static int diameter(Node root){
+        if(root == null){
+            return 0;
+        }
+        
+        int diam1 = diameter(root.left);
+        int diam2 = diameter(root.right);
+        int diam3 = height(root.left)+height(root.right)+1;
+        return Math.max(diam3,Math.max(diam1,diam2));
+    }
+
+    static class TreeInfo{
+        int ht;
+        int diam;
+
+        TreeInfo(int ht,int diam){
+            this.ht = ht;
+            this.diam= diam;
+        }
+    }
+
+    public static TreeInfo diameter2(Node root){
+        
+        if(root == null){
+            return new TreeInfo(0,0);
+        }
+
+        TreeInfo left = diameter2(root.left);
+        TreeInfo right = diameter2(root.right);
+
+        int myHeight = Math.max(left.ht,right.ht)+1;
+        int diam1 = left.diam;
+        int diam2 = right.diam;
+        int diam3 = left.ht+right.ht+1;
+
+        int mydiam = Math.max(Math.max(diam1,diam2),diam3);
+
+        TreeInfo myInfo = new TreeInfo(myHeight,mydiam);
+
+        return myInfo;
+    }
+
     public static void main(String[] args){
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1,6,-1,-1};
 
@@ -98,6 +176,11 @@ public class BuildPreOrderTree {
         //preOrder(root);
         //inOrder(root);
         //postOrder(root);
-        levelOrder(root);
+        //levelOrder(root);
+        System.out.println(countOfNodes(root));
+        System.out.println(sumOfNodes(root));
+        System.out.println(height(root));
+        System.out.println(diameter(root));
+        System.out.println(diameter2(root).diam);
     }
 }
